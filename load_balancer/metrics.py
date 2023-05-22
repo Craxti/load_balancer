2807c8ab7009a8cb710264129d2b5ce1dbcd7830
Aleksandr Fetisov <alexgette17@gmail.com>
2023-05-22 11:38:08 +0400

    uodt

--- load_balancer/metrics.py
+++ load_balancer/metrics.py
@@ -1,32 +1,37 @@
-import time
 import random
-from prometheus_client import Counter, Histogram, Gauge
+from prometheus_client import Counter, Histogram, Gauge, CollectorRegistry, start_http_server
 
 
 class Metrics:
     def __init__(self):
-        self.request_counter = Counter('load_balancer_requests_total', 'Total Requests')
-        self.response_time_histogram = Histogram('load_balancer_response_time_seconds', 'Response time per second')
-        self.connections_counter = Counter('load_balancer_active_connections_total', 'Total number of active connections')
-        self.throughput_gauge = Gauge('load_balancer_throughput_bytes', 'Data transfer bandwidth in bytes')
-        self.resource_usage_gauge = Gauge('load_balancer_resource_usage_percent', 'Percentage of System Resource Usage')
-        self.anomaly_counter = Counter('load_balancer_anomalies_total', 'Total number of detected anomalies')
+        self.registry = CollectorRegistry()
+        self.request_counter = Counter('load_balancer_requests_total', 'Total Requests', registry=self.registry)
+        self.response_time_histogram = Histogram('load_balancer_response_time_seconds', 'Response time per second',
+                                                     registry=self.registry)
+        self.connections_counter = Counter('load_balancer_active_connections_total',
+                                               'Total number of active connections', registry=self.registry)
+        self.throughput_gauge = Gauge('load_balancer_throughput_bytes', 'Data transfer bandwidth in bytes',
+                                          registry=self.registry)
+        self.resource_usage_gauge = Gauge('load_balancer_resource_usage_percent',
+                                              'Percentage of System Resource Usage', registry=self.registry)
+        self.anomaly_counter = Counter('load_balancer_anomalies_total', 'Total number of detected anomalies',
+                                           registry=self.registry)
 
     def setup(self):
         # Initializing the metric collection system (for example, connecting to Prometheus or Graphite)
-        pass
+        start_http_server(8000)
 
     def update_metrics(self, response):
         self.request_counter.inc()
-        response_time = random.uniform(0.1, 0.5)  # Example: random response time from VPS
+        response_time = random.uniform(0.1, 0.5)
         self.response_time_histogram.observe(response_time)
 
         # Additional metrics
         self.connections_counter.inc()
-        data_throughput = random.uniform(100, 1000)  # Example: random data transfer bandwidth
+        data_throughput = random.uniform(100, 1000)
         self.throughput_gauge.set(data_throughput)
 
-        resource_usage = random.uniform(0, 100)  # Example: random system resource usage
+        resource_usage = random.uniform(0, 100)
         self.resource_usage_gauge.set(resource_usage)
 
         # Perform metric analysis and aggregation to gain valuable insight into VPS status and system performance
@@ -34,15 +39,14 @@ class Metrics:
 
     def analyze_metrics(self):
         # Retrieve metric values for analysis
-        average_response_time = self.response_time_histogram.observe()
+        average_response_time = self.response_time_histogram.observe(0)
         active_connections = self.connections_counter._value.get()
         data_throughput = self.throughput_gauge._value.get()
         resource_usage = self.resource_usage_gauge._value.get()
 
         # Analyze metrics for anomalies and performance optimization
-        if average_response_time > 1.0:
+        if average_response_time is not None and average_response_time > 1.0:
             self.send_notification("Average response time exceeded!")
-            self.anomaly_counter.inc()
 
         if active_connections > 1000:
             self.send_notification("Exceeded allowed number of active connections!")
--- load_balancer/utils.py
+++ load_balancer/utils.py
@@ -1,15 +1,17 @@
 import os
 import time
 
+
 def create_directory(directory):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    os.makedirs(directory, exist_ok=True)
+
 
 def get_timestamp():
     return int(time.time())
 
+
 def format_bytes(size):
-    power = 2**10
+    power = 2 ** 10
     n = 0
     size_labels = {0: '', 1: 'KB', 2: 'MB', 3: 'GB', 4: 'TB'}
 
